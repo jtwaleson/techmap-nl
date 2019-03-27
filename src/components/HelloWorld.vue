dd18697109952217bd36e89e367b713835141660
Jouke Waleson <jouke@easee.online>
2019-03-27 23:09:55 +0100

    add a map

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,82 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <gmap-map :center="center" :options="options" :zoom="10" style="width: 500px; height: 500px">
+            <gmap-cluster>
+                <gmap-marker v-for="(m, index) in markers"
+                             :position="m.position"
+                             :title="m.text"
+                             :clickable="true"
+                             :draggable="false"
+                             @click="center=m.position"
+                             :key="index"
+                             ></gmap-marker>
+            </gmap-cluster>
+        </gmap-map>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    name: 'HelloWorld',
+    data() {
+        return {
+            center: {
+                lat: 52.101,
+                lng: 5.1,
+            },
+            options: {
+                clickableIcons: false,
+                fullscreenControl: false,
+                minZoom: 8,
+                rotateControl: false,
+                panControl: false,
+                restriction: {
+                    latLngBounds: {
+                        north: 53.7,
+                        south: 50.7,
+                        west: 3,
+                        east: 7.6,
+                    },
+                    strictBounds: true,
+                },
+                mapTypeId: "terrain",
+                streetViewControl: false,
+            },
+            markers: [
+            {
+                position: {lat: 52.111287, lng: 5.111602},
+                text: 'x'
+            }, {
+                position: {lat: 52.091287, lng: 5.111602},
+                text: 'y'
+            }, {
+                position: {lat: 52.338977, lng: 4.913475},
+                text: 'z'
+            }
+            ]
+        };
+    },
+    props: {
+        msg: String
+    }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+    margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+    display: inline-block;
+    margin: 0 10px;
 }
 a {
-  color: #42b983;
+    color: #42b983;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as VueGoogleMaps from 'vue2-google-maps';
+import GmapCluster from 'vue2-google-maps/dist/components/cluster';
 
 Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyBJagTu45RHP0E1EBMERlO5cvrjCRpuXzw',
+    //libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+
+    //// If you want to set the version, you can do so:
+    // v: '3.26',
+  },
+});
+
+Vue.component('GmapCluster', GmapCluster);
 
 new Vue({
   router,
